9d3cfb844c9645739c2e589f31bc574222cbaf7b
rink2 <kaikyp@protonmail.com>
2026-07-29 19:07:19 -0300

    Update turnos.gd

--- turnos.gd
+++ turnos.gd
@@ -2,17 +2,17 @@ extends Node2D
 
 var HPatual = 50
 var maxHP_jogador = 50
-var dano 
+var dano_j 
+
+var HPinimigo = 50
+var maxHP_inimigo = 50 
+var dano_i 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	setHealth($Jogador/HPBar,HPatual, maxHP_jogador)
+	setHealth($Jogador/HPBar,HPinimigo, maxHP_inimigo)
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
 
 func setHealth(HPBar, HP, maxHP):
 	HPBar.max_value = maxHP
