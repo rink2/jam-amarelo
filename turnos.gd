ab05676d6806f971cd6a940340f501206a591e53
rink2 <kaikyp@protonmail.com>
2026-07-29 19:39:30 -0300

    Update turnos.gd

--- turnos.gd
+++ turnos.gd
@@ -12,8 +12,22 @@ var dano_i
 func _ready() -> void:
 	setHealth($Jogador/HPBar,HPatual, maxHP_jogador)
 	setHealth($Jogador/HPBar,HPinimigo, maxHP_inimigo)
-	pass # Replace with function body.
+	
+	while HPatual>0 and HPinimigo>0:
+		#ataqueinimigo
+		HPatual = HPatual - dano_i 
+		setHealth($Jogador/HPBar,HPatual, maxHP_jogador)
+		if HPatual>0 and HPinimigo>0:
+			#escolha jogador
+				#ataque fraco
+				#ataque forte
+			pass
+			setHealth($Jogador/HPBar,HPinimigo, maxHP_inimigo)
+		else: break
+	
+
 
 func setHealth(HPBar, HP, maxHP):
 	HPBar.max_value = maxHP
 	HPBar.value = HP
+
